509d6428e6cf2fb99ac6ade99d1c42fe2f68c3f9
prashant <prashant.gupta@surewaves.com>
2020-07-19 20:06:13 +0530

    before assignment two

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -6,4 +6,10 @@ import {Component} from '@angular/core';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus = 'offline';
+
+  getServerStatus(): string {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,10 +7,26 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
+  allowNewServer = false;
+  serverCreatedStatus = 'No Server Created Yet';
+  numberOfServerCreated = 0;
+  servername = 'prasha';
+
   constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
   ngOnInit(): void {
   }
 
+  onServerCreation(): void {
+    this.numberOfServerCreated++;
+    this.serverCreatedStatus = this.numberOfServerCreated + ' Server Created and last server name was ' + this.servername;
+  }
+
+  onServerNameChange(event: Event): void {
+    this.servername = (event.target as HTMLInputElement).value;
+  }
 }
