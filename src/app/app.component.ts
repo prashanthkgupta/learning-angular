74bb95eeee2010c2739eac5f83cc153f00bf2f4a
prashant <prashant.gupta@surewaves.com>
2020-07-25 16:36:03 +0530

    component deep dive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.Emulated // None, Native
 })
 export class AppComponent {
+  serverElements = [{type: 'server', name: 'test server', content: 'just the test server'}];
+
+  onServerAdded($serverData: { serverName: string, serverContent: string }): void {
+    this.serverElements.push({
+      type: 'server',
+      name: $serverData.serverName,
+      content: $serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded($blueprintData: { blueprintName: string, blueprintContent: string }): void {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: $blueprintData.blueprintName,
+      content: $blueprintData.blueprintContent
+    });
+  }
+
+  changeFirstServer(): void {
+    this.serverElements[0].name = 'Changed';
+  }
+
+  deleteFirstServer(): void {
+    this.serverElements.splice(0, 1);
+  }
+
 }
